72941b6127dd57ff72403145152638f8ba911d7b
mrxk115 <mrxk115@gmail.com>
2025-01-28 15:38:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://panchalishanibigbigpvtltd
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Time in seconds for message delete, put 0 to never delete
-TIME = int(os.environ.get("TIME", "10"))
+TIME = int(os.environ.get("TIME", "1800"))
 
 
 #force sub channel id, if you want enable force sub
